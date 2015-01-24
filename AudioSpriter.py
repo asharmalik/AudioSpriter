0a17591068607dec347c0e3d5e9ff40154a29fa9
Ashar Malik <ashar.malik.m@gmail.com>
2015-01-23 18:44:30 -0600

    Added code generation for Howler.js

--- AudioSpriter.py
+++ AudioSpriter.py
@@ -8,17 +8,55 @@ def get_sound_files():
 
     f = []
 
-    for (dirpath, dirnames, filenames) in walk('C:\Users\Ashar Malik\Dropbox\Coding\Python\AudioSpriter\sounds'):
+    for (dirpath, dirnames, filenames) in walk(os.getcwd()+"\\sounds"):
         f.extend(filenames)
         break
 
     return f
 
-def create_sound_sprite():
+def create_sound_sprite(filetype='ogg', gen_code=False):
     files = get_sound_files()
-    current_dir = os.getcwd()+"\\"
+    current_dir = os.getcwd()+"\\sounds\\"
+    export_name = "spritesheet"+"."+filetype
+
+    sounds = []
+    code = "var sound = new Howl({\n\turls: ['"+export_name+"'],\n\tsprite: {\n"
+    current_time = 0
 
     for file in files:
-        pass
+        if str(file).__contains__('.'+filetype): #filter to only the filetype requested
+            file_name = file.split("\\").pop().split(".")[0]
 
-ogg_version = AudioSegment.from_ogg("C:\Users\Ashar Malik\Dropbox\Coding\Python\AudioSpriter\sounds\Boss_apfelsaft.ogg")
+            sounds.append(AudioSegment.from_ogg(current_dir+file))
+            duration_ms = sounds[sounds.__len__()-1].duration_seconds*1000
+
+            code+="\t\t"+file_name+": ["+str(current_time)+", "+str(duration_ms)+"]"
+            code+= ("," if files.index(file)<files.__len__()-1 else "") + "\n"
+            current_time+=duration_ms
+
+    code+="\t}\n});"
+
+    if sounds.__len__() < 1:
+        return
+
+
+    spritesheet = sounds.pop(0)
+
+    for sound in sounds:
+        spritesheet = spritesheet + sound
+
+    spritesheet.export("spritesheet\\"+export_name)
+
+    if gen_code:
+        file = open("spritesheet\\sound.js", "w")
+        file.write(code)
+        file.close()
+        print("Generated spritesheet\\sound.js")
+
+    print("Generated "+"spritesheet\\"+export_name)
+
+
+
+create_sound_sprite("ogg", True)
+
+#ogg_version = AudioSegment.from_ogg("C:\Users\Ashar Malik\Dropbox\Coding\Python\AudioSpriter\sounds\Boss_apfelsaft.ogg")
